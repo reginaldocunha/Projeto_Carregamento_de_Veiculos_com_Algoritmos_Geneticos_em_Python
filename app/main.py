1177736bc570fed0dbefeddec4ecd25d72677a52
Reginaldo Cunha <reginaldo.cunhasj@gmail.com>
2024-01-09 23:04:52 -0300

    Importando dados do banco de dados MySQL

--- app/main.py
+++ app/main.py
@@ -6,3 +6,14 @@ class Produto():
         self.espaco = espaco
         self.valor = valor
 
+if __name__ == '__main__':
+    lista_produtos = []
+    conexao = pymysql.connect(host='localhost', user='root', password='@Regi371227', database='produtos')
+    cursor = conexao.cursor()
+    cursor.execute('SELECT nome, espaco, valor, quantidade FROM produtos')
+    for produto in cursor:
+        for i in range(produto[3]):
+            lista_produtos.append(Produto(produto[0], produto[1], produto[2]))
+
+    cursor.close()
+    conexao.close()
