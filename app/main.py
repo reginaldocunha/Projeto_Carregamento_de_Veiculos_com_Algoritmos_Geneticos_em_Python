54f0524e15cc0f640b0ad787af0d0e10373ac64e
Reginaldo Cunha <reginaldo.cunhasj@gmail.com>
2024-01-09 23:21:11 -0300

    Implementação da class Individuo

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
 import pymysql
+from random import random
+
 
 class Produto():
     def __init__(self, nome, espaco, valor):
         self.nome = nome
         self.espaco = espaco
         self.valor = valor
+
+class Individuo():
+    def __init__(self, espacos, valores, limite_espacos, geracao=0)
+        self.espacos = espacos
+        self.valores = valores
+        self.limite_espacos = limite_espacos
+        self.nota_avaliacao = 0
+        self.geracao = geracao
+        self.cromossomo = []
+
+        # seleciona qual produto sera carregado
+        for i in range(len(espacos)):
+            if random() < 0.5:   # 0.5 significa que ha 50% de chance de dar 0 e 50% de chance de dar 1
+                self.cromossomo.append('0')
+            else:
+                self.cromossomo.append('1')
+
+
 
 if __name__ == '__main__':
     lista_produtos = []
