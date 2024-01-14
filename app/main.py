1952e78f3b8da3cfba8107209cd9d4ab7cc3b5a9
Reginaldo Cunha <reginaldo.cunhasj@gmail.com>
2024-01-14 00:53:41 -0300

    Implantando a função resolver

--- app/main.py
+++ app/main.py
@@ -107,6 +107,51 @@ class AlgoritmoGenetico():
                                                                melhor.espaco_usado,
                                                                melhor.cromossomo))
 
+    def resolver(self, taxa_mutacao, numero_geracoes, espacos, valores, limite_espacos):
+        self.inicializa_populacao(espacos, valores, limite_espacos)
+
+        for individuo in self.populacao:
+            individuo.avaliacao()
+
+        self.ordena_populacao()
+        self.melhor_solucao = self.populacao[0]
+        self.lista_solucoes.append(self.melhor_solucao.nota_avaliacao)
+
+        self.visualiza_geracao()
+
+        for geracao in range(numero_geracoes):
+            soma_avaliacao = self.soma_avaliacoes()
+            nova_populacao = []
+
+            for individuos_gerados in range(0, self.tamanho_populacao, 2):
+                pai1 = self.seleciona_pai(soma_avaliacao)
+                pai2 = self.seleciona_pai(soma_avaliacao)
+
+                filhos = self.populacao[pai1].crossover(self.populacao[pai2])
+
+                nova_populacao.append(filhos[0].mutacao(taxa_mutacao))
+                nova_populacao.append(filhos[1].mutacao(taxa_mutacao))
+
+            self.populacao = list(nova_populacao)
+
+            for individuo in self.populacao:
+                individuo.avaliacao()
+
+            self.ordena_populacao()
+
+            self.visualiza_geracao()
+
+            melhor = self.populacao[0]
+            self.lista_solucoes.append(melhor.nota_avaliacao)
+            self.melhor_individuo(melhor)
+
+        print("\nMelhor solução -> G: %s Valor: %s Espaço: %s Cromossomo: %s" %
+              (self.melhor_solucao.geracao,
+               self.melhor_solucao.nota_avaliacao,
+               self.melhor_solucao.espaco_usado,
+               self.melhor_solucao.cromossomo))
+
+        return self.melhor_solucao.cromossomo
 
 
 if __name__ == '__main__':
